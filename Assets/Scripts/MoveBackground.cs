44cb3157975a41acd2b1b5051f36e4bd34e3e191
hyde1357 <56694315+hyde1357@users.noreply.github.com>
2020-05-05 16:29:36 +0300

    Initial commit

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+
+{
+    [SerializeField] float speed = 0.02f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
